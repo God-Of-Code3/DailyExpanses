5fc9a94a80e11a76addd9dcdff6d91a1b4aa2984
Лебедев Александр <sashaleb2006@yandex.ru>
2022-01-30 18:59:49 +0300

    Создание таблицы транзакции

---
+++ database/migrations/2022_01_30_155030_create_transactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+use App\Models\Category;
+use App\Models\User;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->double('sum', 11, 2);
+            $table->foreignIdFor(User::class);
+            $table->foreignIdFor(Category::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
